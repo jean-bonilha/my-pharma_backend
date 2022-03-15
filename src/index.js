7a4992e6a2d5af844835628dd259881fe3697367
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2022-03-15 01:40:29 -0400

    develop(C10):run "standard" command with --fix flag

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
 app.listen(3000, () => {
-  console.log('Listening to port 3000');
-});
+  console.log('Listening to port 3000')
+})
